eb47ccb7b0841319ecf961d67ba1245aefe1195c
Raphdf201 <raphdf201@hotmail.com>
2025-09-23 19:43:12 -0400

    Switch to lazyvim

--- init.lua
+++ init.lua
@@ -1,27 +1,2 @@
--- Disable perl health warning
-vim.g.loaded_perl_provider = 0
-
--- Enable syntax and filetype detection
-vim.cmd("syntax on")
-vim.cmd("filetype plugin indent on")
-vim.opt.number = true
-
--- Bootstrap lazy.nvim
-local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system({
-    "git", "clone", "--filter=blob:none",
-    "https://github.com/folke/lazy.nvim.git",
-    "--branch=stable", lazypath,
-  })
-end
-vim.opt.rtp:prepend(lazypath)
-
--- Load plugins
-require("keybinds")
-require("lazy").setup("plugins")
-require("java").setup {
-	-- jdtls config
-}
-require("jdtlsConfig")
-
+-- bootstrap lazy.nvim, LazyVim and your plugins
+require("config.lazy")
--- lua/keybinds.lua
+++
@@ -1,7 +0,0 @@
-vim.g.mapleader = " "
-vim.keymap.set("n", "<leader>w", ":w<LF>", { desc = "Save" })
-vim.keymap.set("n", "<leader>q", ":wq<LF>", { desc = "Save and quit" })
-vim.keymap.set("n", "<leader>a", ":qa!<LF>", { desc = "Quit and abandon changes" })
-vim.keymap.set("n", "<leader>cd", ":Ex<LF>", { desc = "Save and go back to directory listing" })
-vim.keymap.set("n", "<leader>p", ":!git push", { desc = "Push current branch to origin" })
-
