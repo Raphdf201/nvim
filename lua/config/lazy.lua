4fd36024344ca21ea044b9aa03e2c4afa120e7da
Raphdf201 <raphdf201@hotmail.com>
2025-09-23 21:49:18 -0400

    Start customizing lazyvim

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -20,6 +20,8 @@ require("lazy").setup({
     -- add LazyVim and import its plugins
     { "LazyVim/LazyVim", import = "lazyvim.plugins" },
     -- import/override with your plugins
+    { import = "lazyvim.plugins.extras.lang.json" },
+    { import = "lazyvim.plugins.extras.ui.mini-animate" },
     { import = "plugins" },
     { import = "lazyvim.plugins.extras.dap.core" },
   },
